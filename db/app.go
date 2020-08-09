612942102c39d6f60de82eae8e647ee68b9b21ac
bsnuag <bishnuagrawal1765@gmail.com>
2020-08-09 15:24:36 +0530

    add embedded mode in blade app

--- db/app.go
+++ db/app.go
@@ -3,20 +3,36 @@ package main
 import (
 	"bladedb"
 	"bladedb/server"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		panic("No config file passed, exiting db")
+	configPath := flag.String("config", "", "db config file path")
+	embeddedMode := flag.Bool("embed-mode", true, "db mode")
+	flag.Parse()
+
+	_, err := os.Stat(*configPath)
+	if os.IsNotExist(err) {
+		log.Fatalf(fmt.Sprintf("could not locate config file: %s", *configPath))
 	}
-	configPath := os.Args[1]
-	bladedb.Open(configPath)
-	fmt.Println(fmt.Sprintf("starting bladedb server, listening on %s", bladedb.ServerAddress))
-	go server.StartServer(bladedb.ServerAddress)
+	bladedb.Open(*configPath)
+	if !*embeddedMode {
+		log.Println("Starting blade in Server mode")
+		go func() {
+			log.Println(fmt.Sprintf("starting bladedb server, listening on %s", bladedb.ServerAddress))
+			err := server.StartServer(bladedb.ServerAddress)
+			if err != nil {
+				log.Fatalf(fmt.Sprintf("error while staring blade db, error: %v", err))
+			}
+		}()
+	} else {
+		log.Println("Starting blade in Embedded mode")
+	}
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGUSR1) //kill -SIGUSR1 pId
@@ -24,7 +40,7 @@ func main() {
 	// flush all memtable and wait for all compaction to complete - will be replaced with cool cli tool like nodetool in cassandra
 	for {
 		sig := <-sigs
-		fmt.Println(fmt.Sprintf("received signal: %v to stop bladedb", sig))
+		log.Println(fmt.Sprintf("received signal: %v to stop bladedb", sig))
 		server.StopServer()
 		bladedb.Drain()
 		break
